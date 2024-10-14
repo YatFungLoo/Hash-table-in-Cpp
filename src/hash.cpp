4bf42e631361c02b5c27649bda9e6028b80d92f4
YatFungLoo <looyatfung@gmail.com>
2024-10-14 20:36:56 +0800

    boilerplate files

--- src/hash.cpp
+++ src/hash.cpp
@@ -1,4 +1,4 @@
-#include "header.hpp"
+#include "hash.hpp"
 
 void Example::Print() {
     std::cout << "Hello World." << std::endl;
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "header.hpp"
+#include "hash.hpp"
 #include <cassert>
 
 int main() {
